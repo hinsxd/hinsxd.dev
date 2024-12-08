96ba4308781fa5df650a7e1cd597bb4e586347c9
Alvin Li <hins.xd@hotmail.com>
2024-12-08 16:14:47 +0800

    stash

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,10 @@
 module.exports = {
-  semi: true,
-  printWidth: 80,
-  arrowParens: "always",
-  singleQuote: false,
+  semi: false,
+  singleQuote: true,
+  printWidth: 100,
   tabWidth: 2,
   useTabs: false,
-};
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+}
